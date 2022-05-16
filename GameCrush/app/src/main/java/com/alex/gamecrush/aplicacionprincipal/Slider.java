cd8067cf3eb80f261f7f8f6a36bf4ec9a8e17131
Vasile Alexandru <alexpcfp02@gmail.com>
2022-05-16 14:44:09 +0200

    Añadidos documentos y flecha al slider

--- GameCrush/app/src/main/java/com/alex/gamecrush/aplicacionprincipal/Slider.java
+++ GameCrush/app/src/main/java/com/alex/gamecrush/aplicacionprincipal/Slider.java
@@ -3,16 +3,28 @@ package com.alex.gamecrush.aplicacionprincipal;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.alex.gamecrush.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Slider extends AppCompatActivity {
-
+    FirebaseAuth auth;
+    FirebaseUser user;
     private ViewPager viewPager;
+    // consulta datos
     private SliderAdapter sliderAdapter;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference baseDeDatos;
+    //
+    ImageView flechaAtras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,16 @@ public class Slider extends AppCompatActivity {
         viewPager = findViewById(R.id.slider);
         sliderAdapter = new SliderAdapter(this);
         viewPager.setAdapter(sliderAdapter);
+        flechaAtras = findViewById(R.id.flechaSlider);
+
+        flechaAtras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Slider.this, "Texto", Toast.LENGTH_SHORT).show();
+                //startActivity(new Intent(Slider.this, MainActivity.class));
+                //finish();
+            }
+        });
     }
 
     @Override
